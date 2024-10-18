6acb6e619317f15487adc012755e4ff8ca3d6000
Joha Wilson <johawilsonatu@icloud.com>
2024-10-18 19:26:26 +0100

    Learned about Classes, Methods, if statements,operators

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("Yes,this is an alien!");
+        }
+    }
+}
