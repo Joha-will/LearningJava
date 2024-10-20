79aa22164b1d7613973ab192eecb9e24662b914b
Joha Wilson <johawilsonatu@icloud.com>
2024-10-20 23:43:21 +0100

    Today I learn about the Logical AND and OR operators, The NOT operator and Ternary Operators@

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
 
         boolean isAlien = false;
@@ -7,5 +8,56 @@ public class Hello {
             System.out.println("It is not an alien!");
             System.out.println("Yes,this is an alien!");
         }
+
+        int topScore = 90;
+        if (topScore == 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 80;
+
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("You got the low score!");
+        }
+
+        if ((topScore > 90) || (topScore < 100)){
+            System.out.println("Either or both of the condition is true ");
+        }
+
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("It is not a car!");
+        }
+
+        String makeOfCar = "Vauxhall";
+        boolean isDomestic = makeOfCar == "Vauxhall" ? false : true;
+        if (isDomestic) {
+            System.out.println("It is a domestic!");
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
